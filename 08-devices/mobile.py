6c63d5dc3b17f2c15cd6b619c8c551fd72870a8a
flyingbest <taeyoon4715@gmail.com>
2017-03-04 19:35:02 +0900

    python study #day2 mobile

---
+++ 08-devices/mobile.py
@@ -0,0 +1,17 @@
+# Small devices
+
+# The world is getting samller
+# there's more than just desktop computers out there.
+
+# A quick search of the Web uncovers a pleasent surprise: Python runs on Android.
+# At least a version of Python runs on Android. A project called Scripting Layer for Android(SL4A) provides technology to let you
+# run Python on any Android device. But there's a catch.
+
+# Yes. SL4A ships with Python 2, not 3.
+
+# Set up your deveopment environment
+
+# Download the Software Development Kit (SDK)
+
+# Go home computer, Do it when you complete the download the SDK.
+# Not on linux. on windows.
