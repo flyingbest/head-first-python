72bb5052c521c1cdc6af9baa4bead816226a3cb5
flyingbest <taeyoon4715@gmail.com>
2017-03-04 21:26:24 +0900

    python study #day2 handling

---
+++ 09-handle/handle.py
@@ -0,0 +1,32 @@
+# Handling input
+
+# Create an HTML form template
+"""
+creating file:
+	templates/form.html
+	cgi-bin/process-time.py			//cgi script chmod +x		//no need to.
+
+write the code for two func to add to yate.py
+	create_inputs()
+	do_form()	
+
+creating file:
+	cgi-bin/test_form.py			//cgi script chmod +x
+
+Enter the URL for the CGI script :
+	http://localhost:8080/cgi-bin/test_form.py
+"""
+
+# The data is delivered to your CGI script
+"""
+creating file:
+	new CGI script called add_timing_data.py		//cgi script chmod +x
+
+Enter the URL for the CGI script :
+	http://localhost:8080/cgi-bin/test_form.py
+
+Input some data into your web form. And check the web browser.
+The web server's logging screen displays the data that arrived, as well as the name associated with it. 
+"""
+
+# Next, experience on an Android phone.
